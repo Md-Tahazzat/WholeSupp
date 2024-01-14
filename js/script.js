845288eca012bf8e3c7487b669a016c9c94e1a56
Md-Tahazzat <mdtahazzatali820@gmail.com>
2024-01-14 20:25:13 +0600

    menu toggle option added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+const navlist = document.querySelector("#primary-nav-list");
 
+const closeButton = document.querySelector(".nav-menu-close-btn");
+closeButton.addEventListener("click", toggleMenu);
+
+const menuOpenButton = document.querySelector(".nav-menu-btn");
+menuOpenButton.addEventListener("click", () => toggleMenu("open"));
+
+const navlinks = document.querySelectorAll("#primary-nav-list li");
+navlinks.forEach((link) => link.addEventListener("click", toggleMenu));
+
+function toggleMenu(action) {
+  if (action === "open") return (navlist.style.transform = "translateX(0)");
+  navlist.style.transform = "translateX(-100%)";
+}
+
+window.addEventListener("resize", () => {
+  const windowWidth = window.innerWidth;
+  if (windowWidth >= 1024) {
+    toggleMenu("open");
+  }
+});
